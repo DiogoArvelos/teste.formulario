0ac4431c5bd99595b380c76de6ba48f18a743822
Diogo <147043155+DiogoArvelos@users.noreply.github.com>
2026-03-12 17:07:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -150,6 +150,7 @@ def salvar():
 # ==========================================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
